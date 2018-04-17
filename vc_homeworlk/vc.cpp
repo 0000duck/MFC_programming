4b3829923380eebe5872fb40f7f2dd055fef99e7
chanchann <www.769440972@qq.com>
2018-04-17 22:24:06 +0800

    update

---
+++ vc_homeworlk/vc.cpp
@@ -0,0 +1,29 @@
+## if-else 
+
+if(x>5)
+{
+    cout<<"The x is bigger than 5"<<end;
+}
+else if(x!=5)
+{
+    cout<<"The x is not equal to 5"<<endl;
+}
+else 
+{
+    cout<<"The x is smaller than 5"<<endl;
+}
+
+## switch 
+
+switch(x)
+{
+    case 5:
+        cout<<"The number is 5"<<endl;
+        break;
+    case 6:
+        cout<<"The number is 6"<<endl;
+        break;
+    default:
+        cout<<"THe number is not equal to 5 or 6"<<endl;
+}
+
